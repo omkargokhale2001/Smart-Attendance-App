fc6f622ae98e531efa6554a55465aa5f2296257b
Omkar <omkargokhalepune@gmail.com>
2022-08-22 20:03:41 +0530

    All api code push

---
+++ main.py
@@ -0,0 +1,139 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from pymongo import MongoClient
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+import pymongo
+from model import *
+
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World'
+
+
+@app.route('/mark_attendance', methods=['GET', 'POST'])
+def mark_attendance():
+    if request.method == 'POST':
+        grp_image = request.form['grp_image']
+        # grp_image = cv2.imread("./test_data/Group Photo (1).png")
+        grp_image = np.array(grp_image)
+        CONNECTION_STRING = "mongodb+srv://syntaxico:{i2hhW_Z5@cluster0.fx786w6.mongodb.net/?retryWrites=true&w=majority"
+        # CONNECTION_STRING = "mongodb+srv://omkar:omkar1212@cluster1.2melkie.mongodb.net/?retryWrites=true&w=majority"
+        client = MongoClient(CONNECTION_STRING)
+        db_name = client["SIH_app_database"]
+        users = db_name["Workers"]
+        info_cur = list(users.find({}, {
+            "name": 1,
+            "face_embedding": 1
+        }))
+
+        attendance_list = list(set(wrapper(grp_image, info_cur)))
+
+        for worker in attendance_list:
+            users.update_one({"name": str(worker)},
+                             {
+                "$inc": {"days_present": 1}
+            })
+
+        users.update_many({}, {
+            "$inc": {"total_days": 1}
+        })
+        # optional -> if to < 30/getMonth:total_day = 0
+
+    return attendance_list
+
+
+@app.route('/display_percentage', methods=['GET', 'POST'])
+def display_percentage():
+    if request.method == 'GET':
+        CONNECTION_STRING = "mongodb+srv://syntaxico:{i2hhW_Z5@cluster0.fx786w6.mongodb.net/?retryWrites=true&w=majority"
+        # CONNECTION_STRING = "mongodb+srv://omkar:omkar1212@cluster1.2melkie.mongodb.net/?retryWrites=true&w=majority"
+        client = MongoClient(CONNECTION_STRING)
+        db_name = client["SIH_app_database"]
+        users = db_name["Workers"]
+
+        data = list(users.find({},
+                               {
+            "name": 1,
+            "days_present": 1,
+            "total_days": 1,
+        }
+        ))
+
+        res = []
+        percentage_dict = {}
+        for w in data:
+            percentage_dict[w['name']] = (w['days_present'] / w['total_days']) * 100
+
+        return percentage_dict
+
+
+@app.route("/add_worker")
+def add_worker():
+    if request.method == "POST":
+        name = request.form['name']
+        image = request.form['image']
+        embeds = convert_image(image)
+
+        # image1 = cv2.imread("./test_data/Image_10.png")
+        # image1 = np.array(image1)
+        # print(image1)
+        # print(type(image1))
+        # print(type(image1[0]))
+        # embeds = convert_image(image1)
+        # print(len(embeds))
+
+        # Code for adding worker to db
+        CONNECTION_STRING = "mongodb+srv://syntaxico:{i2hhW_Z5@cluster0.fx786w6.mongodb.net/?retryWrites=true&w=majority"
+        # CONNECTION_STRING = "mongodb+srv://omkar:omkar1212@cluster1.2melkie.mongodb.net/?retryWrites=true&w=majority"
+        client = MongoClient(CONNECTION_STRING)
+        db_name = client["SIH_app_database"]
+        users = db_name["Workers"]
+
+        item_2 = {
+            "name": name,
+            "days_present": 0,
+            "total_days": 0,
+            # "face_embedding": request.form.get('face_embd'),
+            "face_embedding": embeds,
+        }
+        users.insert_one(item_2)
+
+        return "Worker added!"
+
+
+@app.route("/delete_worker")
+def delete_worker():
+    # Code for deleting worker from db
+    if request.method == "POST":
+        name = request.form['name']
+        CONNECTION_STRING = "mongodb+srv://syntaxico:{i2hhW_Z5@cluster0.fx786w6.mongodb.net/?retryWrites=true&w=majority"
+        # CONNECTION_STRING = "mongodb+srv://omkar:omkar1212@cluster1.2melkie.mongodb.net/?retryWrites=true&w=majority"
+        client = MongoClient(CONNECTION_STRING)
+        db_name = client["SIH_app_database"]
+        users = db_name["Workers"]
+        users.delete_one({"name": name})
+        return "Worker deleted!"
+
+
+# @app.route("/clear_days")
+# def clear_days():
+#     if request.method == "GET":
+#         #CONNECTION_STRING = "mongodb+srv://syntaxico:{i2hhW_Z5@cluster0.fx786w6.mongodb.net/?retryWrites=true&w=majority"
+#         CONNECTION_STRING = "mongodb+srv://omkar:omkar1212@cluster1.2melkie.mongodb.net/?retryWrites=true&w=majority"
+#         client = MongoClient(CONNECTION_STRING)
+#         db_name = client["SIH_app_database"]
+#         users = db_name["Workers"]
+#
+#         users.update_many({},
+#                           {"$set": {"days_present": 0, "total_days": 0}}
+#                           )
+#         return "days cleared!"
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ model.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from deepface import DeepFace
+import os
+import face_recognition
+import numpy as np
+
+
+def detect_faces(grp_img):
+    face_locations = face_recognition.face_locations(grp_img)
+    images_array = []
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        face_array = grp_img[top-100:bottom+100, left-100:right+100]
+        images_array.append(face_array)
+    return images_array
+
+def convert_image(image):
+    result = DeepFace.represent(img_path=image, model_name="Dlib", enforce_detection=False)
+    return result
+
+
+def find_embeds(image_arr):
+    embed_arr = []
+    for i in range(len(image_arr)):
+        result = DeepFace.represent(img_path=image_arr[i], model_name="Dlib", enforce_detection=False)
+        embed_arr.append(result)
+    return embed_arr
+
+
+def collect_embeds(cur):
+    names = []
+    face_embeds = []
+    for i in cur:
+        names.append(i['name'])
+        face_embeds.append(i['face_embedding'])
+    db_dict = dict(zip(names, face_embeds))
+    return db_dict
+
+
+def is_same_person(img1, img2):
+    em1 = np.array(img1)
+    em2 = np.array(img2)
+    a = np.matmul(np.transpose(em1), em2)
+    b = np.sum(np.multiply(em1, em1))
+    c = np.sum(np.multiply(em2, em2))
+    score = 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+    return score
+
+
+def wrapper(grp_img, cur):
+    img_arr = detect_faces(grp_img)
+    embeds_arr = find_embeds(img_arr)
+    db_dict = collect_embeds(cur)
+    attendance = []
+    db_dict_1 = sorted(db_dict)
+    db_dict = {key:db_dict[key] for key in db_dict_1}
+    names = list(db_dict.keys())
+    embeds = list(db_dict.values())
+    scores = []
+    for i in range(len(embeds_arr)):
+        temp_arr = []
+        for j in embeds:
+            score = is_same_person(embeds_arr[i],j)
+            temp_arr.append(score)
+        scores.append(temp_arr)
+        print(temp_arr)
+        min_ind = np.argmin(temp_arr)
+        attendance.append(names[min_ind])
+    return attendance
+
